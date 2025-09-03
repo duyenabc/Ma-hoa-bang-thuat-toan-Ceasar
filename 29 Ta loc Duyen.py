771ada665f994a2c078217f24a9e9ec8371aa7d6
duyenabc <locduyent@gmail.com>
2025-09-03 12:04:59 +0700

    Add files via upload

--- 29 Ta loc Duyen.py
+++ 29 Ta loc Duyen.py
@@ -1,11 +1,9 @@
 def mahoaCeasar(text, k):
     kq = ""
     k = k % 26
-    # Bước 1: Kiểm tra hợp lệ (chỉ cho chữ cái và dấu cách)
     for char in text:
         if not (char.isalpha() or char == " "):
             return "Không hợp lệ, vui lòng nhập lại."
-    # Bước 2: Mã hóa từng ký tự
     for char in text:
         if char == " ":
             kq += " "
